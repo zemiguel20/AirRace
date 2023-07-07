7a11f65acccb138f90fb56b5a4be1e110a9b24f3
zemiguel20 <jmmscoutinho@gmail.com>
2023-07-07 20:50:11 +0200

    Added thrust and drag forces

--- AircraftBody3D.gd
+++ AircraftBody3D.gd
@@ -1,11 +1,17 @@
 class_name AircraftBody3D
-
 extends RigidBody3D
 
 @export
-var rotation_h_speed = PI*0.1
+## Thrust acceleration
+var thrust_power = 100
+
 @export
-var rotation_v_speed = PI*0.1
+var drag_coefficient = 1
+@export
+## Angle of attack makes the drag stronger
+## This curve gives a multiplier depending on AoA
+## X- Normalized angle, Y- Drag scale
+var drag_aoa_curve: Curve 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,15 +20,32 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var target: Vector3 = Vector3(0, 0, 1);
-	if Input.is_key_pressed(KEY_A):
-		target.x -= 1
-	if Input.is_key_pressed(KEY_D):
-		target.x += 1
-	if Input.is_key_pressed(KEY_W):
-		target.y += 1
-	if Input.is_key_pressed(KEY_S):
-		target.y -= 1
+	pass
+
+func _physics_process(delta):
 	
-	Vector3.FORWARD
-	# ROTATE
+	var velocity_square = linear_velocity.length_squared()
+	# ANGLE OF ATTACK
+	var aoa = linear_velocity.normalized().angle_to(transform.basis.z)
+	var aoa_norm = aoa / PI
+	# Calculate G-force
+	
+	# THRUST
+	var thrust_direction = transform.basis.z
+	var thrust: Vector3 = thrust_power * thrust_direction
+	
+	# DRAG
+	var drag_direction = -linear_velocity.normalized()
+	var drag_multiplier = drag_aoa_curve.sample(aoa_norm)
+	var drag: Vector3 = velocity_square/2 * drag_coefficient * drag_multiplier * drag_direction
+	
+	
+	
+	# Calculate Lift (v²/2 * lift_coefficient * lift_multiplier)
+	# Normalized Curve AoA-Coefficient. Multiplier to edit lift strength
+	
+	# Calculate Induced Drag (lift_coefficient² * induced_drag_constant)
+	
+	var integrated_force: Vector3 = thrust + drag
+	apply_central_force(integrated_force * delta)
+	print(linear_velocity.length())
