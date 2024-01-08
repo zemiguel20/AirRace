4e8998d2441871a55a216ff30ccd9b55a7b40c80
zemiguel20 <jmmscoutinho@gmail.com>
2024-01-08 01:39:55 +0100

    Disabled button no longer has animation

--- ui/shared/base_button.gd
+++ ui/shared/base_button.gd
@@ -6,10 +6,12 @@ const ANIM_DURATION = 0.1
 
 
 func _grow():
-	var tween: Tween = create_tween()
-	tween.tween_property(self, "scale", GROW_SCALE, ANIM_DURATION)
+	if not disabled:
+		var tween: Tween = create_tween()
+		tween.tween_property(self, "scale", GROW_SCALE, ANIM_DURATION)
 
 
 func _shrink():
-	var tween: Tween = create_tween()
-	tween.tween_property(self, "scale", Vector2.ONE, ANIM_DURATION)
+	if not disabled:
+		var tween: Tween = create_tween()
+		tween.tween_property(self, "scale", Vector2.ONE, ANIM_DURATION)
