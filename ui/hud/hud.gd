ac306e404f709df92e1621a20e37116e906e8a58
zemiguel20 <jmmscoutinho@gmail.com>
2024-01-20 23:44:08 +0100

    Fixed hud crashing on scene start. Added prototype waypoint

--- ui/hud/hud.gd
+++ ui/hud/hud.gd
@@ -10,16 +10,10 @@ extends CanvasLayer
 	set(new_time):
 		current_time_ms = new_time
 		if current_time_ms < 0: current_time_ms = 0
-		_update_chrono_label()
-		_update_time_difference_label()
 
 
 ## If there is no previous record (first run)
-@export var no_record: bool = false:
-	set(value):
-		no_record = value
-		_update_time_difference_label()
-		_update_record_label()
+@export var no_record: bool = false
 
 
 ## Record time for current gate
@@ -27,15 +21,13 @@ extends CanvasLayer
 	set(new_time):
 		gate_time_ms = new_time
 		if gate_time_ms < 0: gate_time_ms = 0
-		_update_time_difference_label()
 
 
 ## Record time for finishing race
 @export var record_time_ms: int = 0:
 	set(new_time):
 		record_time_ms = new_time
-		if record_time_ms < 0: record_time_ms = 0 # Minimum 0
-		_update_record_label()
+		if record_time_ms < 0: record_time_ms = 0
 
 
 @export_group("Completion")
@@ -46,7 +38,6 @@ extends CanvasLayer
 	set(new_count):
 		total_gate_count = new_count
 		if total_gate_count < 1: total_gate_count = 1 # Always at least 1 gate
-		_update_race_completion_label()
 
 
 ## How many gates the player has checked
@@ -54,11 +45,31 @@ extends CanvasLayer
 	set(new_count):
 		current_gate_count = new_count
 		current_gate_count = clamp(current_gate_count, 0, total_gate_count)
-		_update_race_completion_label()
+
+
+@export_group("Waypoint")
+
+
+@export var waypoint_enabled := false:
+	set(value):
+		waypoint_enabled = value
+		%Waypoint.visible = waypoint_enabled
+
+## Global position of the target gate in the race
+@export var target_gate_pos := Vector3.ZERO
+
+
+func _process(delta: float) -> void:
+	_update_chrono_label()
+	_update_time_difference_label()
+	_update_record_label()
+	_update_race_completion_label()
+	_update_waypoint()
 
 
 func _update_chrono_label():
-	%Chrono.text = _time_ms_to_string(current_time_ms)
+	if %Chrono != null:
+		%Chrono.text = _time_ms_to_string(current_time_ms)
 
 
 func _update_time_difference_label():
@@ -86,14 +97,27 @@ func _update_record_label():
 	%RecordTime.label_settings.font_color = Color.DARK_GRAY
 
 
-func _update_race_completion_label():
-	var percentage = (float(current_gate_count) / float(total_gate_count)) * 100
-	%Percentage.text = str(percentage) + "%"
-	
-
 func _time_ms_to_string(time_ms: int) -> String:
 	var minutes:int = time_ms / 60000
 	var seconds:int = (time_ms % 60000) / 1000
 	var miliseconds:int = (time_ms % 60000) % 1000
 	var time_str = "%02d:%02d.%03d" % [minutes, seconds, miliseconds]
 	return time_str
+
+
+func _update_race_completion_label():
+	var percentage = (float(current_gate_count) / float(total_gate_count)) * 100
+	%Percentage.text = "%d%%" % percentage
+
+
+func _update_waypoint():
+	if waypoint_enabled:
+		%Waypoint.visible = true
+		var current_cam := get_viewport().get_camera_3d()
+		if current_cam != null:
+			# TODO finish this part after level logic
+			%Waypoint.visible = not current_cam.is_position_behind(target_gate_pos)
+			%Waypoint.position = current_cam.unproject_position(target_gate_pos)
+	else:
+		%Waypoint.visible = false
+
