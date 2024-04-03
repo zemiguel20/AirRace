6bd388ef169b858dda6640282350bd6817066220
zemiguel20 <jmmscoutinho@gmail.com>
2024-04-04 01:30:06 +0200

    Changed aircraft controller to same as tutorial base

--- scripts/aircraft.gd
+++ scripts/aircraft.gd
@@ -3,35 +3,33 @@ extends AnimatableBody3D
 ## @tutorial(Simplified Airplane Controller): https://kidscancode.org/godot_recipes/3.x/3d/simple_airplane/
 
 
-@export var thrust: float = 10
-@export var turn_speed: float = 1
-@export var pitch_speed: float = 1
-@export var roll_speed: float = 1
-@export_range(0.0, 10) var angular_drag: float = 0.5 ## 1 - no rotation smoothing, 0 - infinite rotation "drift"
+@export var forward_speed = 20
+@export var pitch_speed = 1.5
+@export var turn_speed = 1.9
+@export var input_response = 8.0
 
-var turn_input: float = 0.0
-var pitch_input: float = 0.0
-var roll_input: float = 0.0
-
-var lin_velocity: Vector3 = Vector3.ZERO
-var ang_velocity: Vector3 = Vector3.ZERO
+var pitch_input = 0.0
+var turn_input = 0.0
 
 
 func _process(delta: float) -> void:
-	pitch_input = Input.get_axis("pitch_down", "pitch_up")
-	turn_input = Input.get_axis("turn_left", "turn_right")
-	roll_input = Input.get_axis("roll_left", "roll_right")
+	pitch_input = lerp(pitch_input, Input.get_axis("pitch_down", "pitch_up"), input_response * delta)
+	turn_input = lerp(turn_input, Input.get_axis("turn_left", "turn_right"), input_response * delta)
 
 
-func _physics_process(delta: float) -> void:
-	# Movement forward
-	lin_velocity = transform.basis.z * thrust
-	var _collision_data = move_and_collide(lin_velocity * delta)
-	# Rotate based on input
-	var target_ang_velocity = Vector3(pitch_input * pitch_speed, turn_input * turn_speed, roll_input * roll_speed)
-	ang_velocity = ang_velocity.lerp(target_ang_velocity, angular_drag * delta)
-	rotate(-transform.basis.x, ang_velocity.x * delta)
-	rotate(-transform.basis.y, ang_velocity.y * delta)
-	rotate(transform.basis.z, ang_velocity.z * delta)
+func _physics_process(delta):
+	transform.basis = transform.basis.rotated(-transform.basis.x, pitch_input * pitch_speed * delta)
+	transform.basis = transform.basis.rotated(Vector3.DOWN, turn_input * turn_speed * delta)
+	transform.basis = transform.basis.orthonormalized()
 	
-	# TODO: Bank turn animation
+	# Bank turn animation
+	var model = $Model as Node3D
+	var wing_col  = $WingCollisionShape as Node3D
+	model.rotation.z = lerp(model.rotation.z, deg_to_rad(45) * turn_input, delta * 3)
+	wing_col.rotation.z = lerp(wing_col.rotation.z, deg_to_rad(45) * turn_input, delta * 3)
+	
+	var velocity = transform.basis.z * forward_speed
+	move_and_collide(velocity * delta)
+	
+	# TODO: stabilize roll if upside down and if input not being pressed
+	# TODO: check if collision
--- scripts/smooth_camera.gd
+++ scripts/smooth_camera.gd
@@ -1,13 +1,15 @@
 extends Camera3D
 ## @tutorial: https://kidscancode.org/godot_recipes/3.x/3d/interpolated_camera/
 
-@export var lerp_speed: float = 3.0
+@export var lerp_speed = 3.0
 @export var target: Node3D
-@export var offset: Vector3 = Vector3.ZERO
+@export var offset = Vector3.ZERO
 
-func _physics_process(delta: float) -> void:
+func _physics_process(delta):
 	if !target:
 		return
-	var target_pos = target.global_transform.translated_local(offset)
-	global_transform = global_transform.interpolate_with(target_pos, lerp_speed * delta)
-	look_at(target.global_transform.origin, Vector3.UP)
+
+	var target_xform = target.global_transform.translated_local(offset)
+	global_transform = global_transform.interpolate_with(target_xform, lerp_speed * delta)
+
+	look_at(target.global_transform.origin, target.transform.basis.y)
