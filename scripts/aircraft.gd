1a10ba25fb61925163a9ce19851fb7e12108acda
zemiguel20 <jmmscoutinho@gmail.com>
2024-04-07 20:02:38 +0200

    Aircraft code simplified/shortened. Bank turn animation linked to input

--- scripts/aircraft.gd
+++ scripts/aircraft.gd
@@ -7,29 +7,25 @@ extends AnimatableBody3D
 @export var pitch_speed = 1.5
 @export var turn_speed = 1.9
 @export var input_response = 8.0
-
 var pitch_input = 0.0
 var turn_input = 0.0
 
 
-func _process(delta: float) -> void:
+func _physics_process(delta):
+	# Read input
 	pitch_input = lerp(pitch_input, Input.get_axis("pitch_down", "pitch_up"), input_response * delta)
 	turn_input = lerp(turn_input, Input.get_axis("turn_left", "turn_right"), input_response * delta)
-
-
-func _physics_process(delta):
-	transform.basis = transform.basis.rotated(-transform.basis.x, pitch_input * pitch_speed * delta)
-	transform.basis = transform.basis.rotated(Vector3.DOWN, turn_input * turn_speed * delta)
-	transform.basis = transform.basis.orthonormalized()
-	
+	# Pitch
+	# TODO: LIMIT ROTATION TO NOT ALLOW UPSIDE DOWN, SO MAX ANGLE IS 90 POINTING UP OR -90 POINTING DOWN
+	rotate(-transform.basis.x, pitch_input * pitch_speed * delta)
+	# Turn
+	rotate(Vector3.DOWN, turn_input * turn_speed * delta)
 	# Bank turn animation
 	var model = $Model as Node3D
 	var wing_col  = $WingCollisionShape as Node3D
-	model.rotation.z = lerp(model.rotation.z, deg_to_rad(45) * turn_input, delta * 3)
-	wing_col.rotation.z = lerp(wing_col.rotation.z, deg_to_rad(45) * turn_input, delta * 3)
-	
+	model.rotation.z = lerp(model.rotation.z, deg_to_rad(45) * turn_input, input_response * delta)
+	wing_col.rotation.z = lerp(wing_col.rotation.z, deg_to_rad(45) * turn_input, input_response * delta)
+	# Move forward / Thrust
 	var velocity = transform.basis.z * forward_speed
-	move_and_collide(velocity * delta)
-	
-	# TODO: stabilize roll if upside down and if input not being pressed
+	var _collision_data = move_and_collide(velocity * delta)
 	# TODO: check if collision
