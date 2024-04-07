ca4d9790a367fde7105c68a4470f4b4bb44edad1
zemiguel20 <jmmscoutinho@gmail.com>
2024-04-07 20:01:18 +0200

    Added look ahead bonus to smooth camera

--- scripts/smooth_camera.gd
+++ scripts/smooth_camera.gd
@@ -1,15 +1,25 @@
 extends Camera3D
 ## @tutorial: https://kidscancode.org/godot_recipes/3.x/3d/interpolated_camera/
 
-@export var lerp_speed = 3.0
-@export var target: Node3D
-@export var offset = Vector3.ZERO
 
-func _physics_process(delta):
+@export var target: Node3D
+@export var offset = Vector3(0, 0.2, -5.0)
+@export var lerp_speed = 3.0
+@export var look_ahead_offset = 8.0
+
+
+func _ready() -> void:
+	if !target:
+		push_warning("Camera has no follow target.")
+
+
+func _physics_process(delta: float) -> void:
 	if !target:
 		return
-
+	
 	var target_xform = target.global_transform.translated_local(offset)
 	global_transform = global_transform.interpolate_with(target_xform, lerp_speed * delta)
-
-	look_at(target.global_transform.origin, target.transform.basis.y)
+	
+	# looking to point ahead of target while turning will result in increased visibility of the turning side
+	var look_ahead_point = target.global_position + (target.basis.z * look_ahead_offset)
+	look_at(look_ahead_point, target.basis.y)
