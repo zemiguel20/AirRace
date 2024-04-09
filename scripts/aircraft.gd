fd94d89e32dbec60574ceebe95a7f88776b9b8ad
zemiguel20 <jmmscoutinho@gmail.com>
2024-04-09 11:35:17 +0200

    Fixed turn direction when upside down. Added invert input setting

--- scripts/aircraft.gd
+++ scripts/aircraft.gd
@@ -7,24 +7,44 @@ extends AnimatableBody3D
 @export var pitch_speed = 1.5
 @export var turn_speed = 1.9
 @export var input_response = 8.0
-var pitch_input = 0.0
-var turn_input = 0.0
+var angular_velocity = Vector3.ZERO
+
 
 
 func _physics_process(delta):
 	# Read input
-	pitch_input = lerp(pitch_input, Input.get_axis("pitch_down", "pitch_up"), input_response * delta)
-	turn_input = lerp(turn_input, Input.get_axis("turn_left", "turn_right"), input_response * delta)
-	# Pitch
-	# TODO: LIMIT ROTATION TO NOT ALLOW UPSIDE DOWN, SO MAX ANGLE IS 90 POINTING UP OR -90 POINTING DOWN
-	rotate(-transform.basis.x, pitch_input * pitch_speed * delta)
-	# Turn
-	rotate(Vector3.DOWN, turn_input * turn_speed * delta)
+	var pitch_input = Input.get_axis("pitch_down", "pitch_up")
+	if Settings.inverted_pitch_input:
+		pitch_input = pitch_input * -1
+	var turn_input = Input.get_axis("turn_left", "turn_right")
+	if Settings.inverted_turn_input:
+		turn_input = turn_input * -1
+	
+	# Target angular velocity based on input
+	var target_angular_velocity = Vector3.ZERO
+	target_angular_velocity.x = pitch_input * pitch_speed
+	target_angular_velocity.y = turn_input * turn_speed
+	
+	# Invert turn direction if aircraft upside down
+	var is_upside_down = Vector3.UP.angle_to(basis.y) > (PI/2)
+	if is_upside_down:
+		target_angular_velocity.y *= -1
+	
+	# Interpolate angular velocity
+	angular_velocity = angular_velocity.lerp(target_angular_velocity, input_response * delta)
+	
+	# Pitch and turn
+	rotate(-transform.basis.x, angular_velocity.x * delta) # Use local Right axis, positive angle = pitch up
+	rotate(Vector3.DOWN, angular_velocity.y * delta) # Use Down axis, positive angle = turn right
+	
 	# Bank turn animation
 	var model = $Model as Node3D
 	var wing_col  = $WingCollisionShape as Node3D
-	model.rotation.z = lerp(model.rotation.z, deg_to_rad(45) * turn_input, input_response * delta)
-	wing_col.rotation.z = lerp(wing_col.rotation.z, deg_to_rad(45) * turn_input, input_response * delta)
+	model.rotation.z = lerpf(model.rotation.z, deg_to_rad(45) * turn_input, input_response * delta)
+	wing_col.rotation.z = lerpf(wing_col.rotation.z, deg_to_rad(45) * turn_input, input_response * delta)
+	
+	# TODO: If upside down, and no input, start stabilization routine
+	
 	# Move forward / Thrust
 	var velocity = transform.basis.z * forward_speed
 	var _collision_data = move_and_collide(velocity * delta)
---
+++ scripts/autoloads/game_settings.gd
@@ -0,0 +1,5 @@
+extends Node
+
+
+var inverted_pitch_input: bool = true
+var inverted_turn_input: bool = false
